fc026c58e22a14d46299b77b99fdc603a19e0e1f
Mambaul Hisam <hisyamsquartz@gmail.com>
2024-05-26 07:26:02 +0700

    new ui

--- src/index.js
+++ src/index.js
@@ -13,40 +13,48 @@ import { AutoRouter } from 'itty-router';
 const router = AutoRouter();
 
 async function template() {
-	const html = `<!DOCTYPE html>
-        <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	const html = `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Bandcamp Scrapper</title>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <title>BDown</title>
-        <style>
-            table, th, td {
-                border: 1px solid black;
-            }
-    
-            th, td {
-                font-size: 1.5vw;
-            }
-    
-            .center {
-                display: flex; 
-                justify-content: center;
-            }
-        </style>
-        </head>
-        <script>
-            document.addEventListener("DOMContentLoaded", function() {
-                const url = document.getElementById("input_url");
-                url.addEventListener("keyup", (event) => {
-                    if (event.keyCode === 13) {
-                        event.preventDefault();
-                        parse();
-                    }
-                });
-            });
-
-            async function parse() {
-                const value = document.getElementById("input_url").value.trim();
+		<style>
+			html, body {
+				margin: 0;
+				height: 100%;
+			}
+	
+			table, tr, td {
+				border: 1px solid;
+				border-color: #01252C;
+				color: #01252C;
+			}
+	
+			thead > tr > td > div {
+				display: flex;
+				justify-content: center;
+			}
+	
+			td {
+				min-width: 15vw;
+			}
+	
+			tbody > tr > td > a {
+				text-decoration: underline;
+				color: #2b8666;
+	
+				> div {
+					display: flex;
+					justify-content: center;
+				}
+			}
+		</style>
+		<script>
+			async function parse() {
+				const value = document.getElementById("input_url").value.trim();
 				const ctn = document.getElementById("content");
 				ctn.innerHTML = '';
 				// ctn.innerHTML = "<span>loading...</span>
@@ -61,30 +69,30 @@ async function template() {
 						const tbl = document.createElement("table");
 						const thead = document.createElement("thead");
 						const tblBody = document.createElement("tbody");
-	
+
 						const headTr = document.createElement("tr");
 						const headTitle = document.createElement("td");
 						const headUrl = document.createElement("td");
 						headTitle.innerText = "Title"
 						headUrl.innerText = "Download Link"
-	
+
 						headTr.appendChild(headTitle);
 						headTr.appendChild(headUrl);
 						thead.appendChild(headTr);
-	
+
 						for (var i = 0; i < trackinfoObject.trackinfo.length; i++) {
 							const row = document.createElement("tr");
-	
+
 							const cell_title = document.createElement("td");
 							const cell_url = document.createElement("td");
 							cell_title.innerText = trackinfoObject.trackinfo[i].title;
 							cell_url.innerHTML = \`<a href=\"\${trackinfoObject.trackinfo[i].file["mp3-128"]}\"}>Download</a>\`;
-	
+
 							row.appendChild(cell_title)
 							row.appendChild(cell_url)
 							tblBody.appendChild(row)
 						}
-	
+
 						tbl.appendChild(thead);
 						tbl.appendChild(tblBody);
 						tbl.setAttribute("border", "1");
@@ -101,19 +109,44 @@ async function template() {
 					loading.innerText = "error: input can't be blank";
 					ctn.appendChild(loading);
 				}
-            }
-        </script>
-        <body>
-        <div style="display: flex; align-items: center; flex-direction: column; padding-top: 15vw;">
-            <div style="display: flex; border: solid; border-radius: 1vw; padding: 0 1vw 0 1vw;">
-                <input id="input_url" style="border-style: none; min-width: 25vw; min-height: 1.5vw; outline: none;" type="text" placeholder="Paste Bandcamp album URL">
-                <div style="display: flex; align-items: center;">
-                    <span id="search_btn" class="material-symbols-outlined" onclick="parse()" style="cursor: pointer;">search</span>
-                </div>
-            </div>
-            <div id="content" style="padding-top: 1vw;"></div>
-        </div>
-        </body>`;
+			}
+	
+			document.addEventListener("DOMContentLoaded", function() {
+				const url = document.getElementById("input_url");
+				url.addEventListener("keyup", (event) => {
+					if (event.keyCode === 13) {
+						event.preventDefault();
+						parse();
+					}
+				});
+			});
+		</script>
+	</head>
+	<body>
+		<div style="display: flex; align-items: center; flex-direction: column; background-color: #01252C; height: 100%;">
+			<div style="display: flex; flex-direction: row; justify-content: center; border: 3px solid; min-width: 100vw; border-radius: 0px 0px 5vh 5vh; border-left: 0; border-top: 0; border-right: 0; background-color: #2b8666;">
+				<div style="padding: 2vh 0vw 2vh 0vw;">
+					<nav style="display: flex; flex-direction: row; justify-content: space-between; min-width: 410px;">
+						<button style="min-width: 200px; min-height: 60px; max-width: 100vw; border-radius: 10vw; border: 3px solid; background-color: #88DF95; border-color: #01252C;">home</button>
+						<button style="min-width: 200px; min-height: 60px; max-width: 100vw; border-radius: 10vw; border: 3px solid; background-color: #88DF95; border-color: #01252C;">history</button>
+					</nav>
+				</div>
+			</div>
+			<div style="display: flex; height: 300px; justify-content: center;">
+				<img src="" alt="logo" width="80%">
+			</div>
+			<div style="display: flex; flex-direction: column; border: 0px solid; min-width: 100vw; border-radius: 90px 90px 0px 0px / 90px 90px 0vh 0vh; min-height: 60vh; justify-content: center; border-bottom: 0; border-left: 0; border-right: 0; align-items: center; background-color: #88DF95;">
+				<div style="display: flex; flex-direction: row; position: fixed; border: 4px solid; border-radius: 10vw; min-height: 1vh; height: 40px; margin-top: -60vh; padding: 0 20px 0 20px; background-color: white; width: 50vw; justify-content: space-between; border-color: #01252C;">
+					<input id="input_url" style="border-style: none; outline: none; min-width: 1vw; width: 100vw;" type="text" placeholder="Paste Bandcamp album URL">
+					<div style="display: flex; align-items: center;">
+						<span id="search_btn" class="material-symbols-outlined" onclick="parse()" style="cursor: pointer;">search</span>
+					</div>
+				</div>
+				<div id="content" style="padding-top: 1vw; padding-bottom: 40vh;"></div>   
+			</div>
+		</div>
+	</body>
+	</html>`;
 
 	return new Response(html, {
 		headers: {
